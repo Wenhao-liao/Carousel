a0e3e525a766a1cdce756ab860a7f75143d5834f
holiday <756787204@qq.com>
2019-11-02 21:40:01 +0800

    第一次提交轮播图

---
+++ slide.js
@@ -0,0 +1,38 @@
+window.onload = function(){
+    let buttons = $('button');
+    let n = 0;                      //当前active的button下标
+    console.log(buttons);
+    //点击跳转到该画面，并且Button高亮
+    for(let i=0;i<buttons.length;i++){
+        $(buttons[i]).on('click',function(){
+            n = i;                                  //用n记录当前第几个，让下一次轮播的时候可以跟上
+            $(container).css({
+                transform:`translateX(` + i*(-350) +`px)`
+            })
+            $(buttons[i]).addClass('active')
+            .siblings('.active').removeClass('active')
+        })
+    }
+    //触发第一次click，使其位移（第一次位移为0）和变红
+    $(buttons[n % 4]).trigger('click')            //这个n%4很关键，让它通过n++可以做到循环轮播
+    //自动播放
+    let timerId = setInterval(function(){
+        n += 1;
+        let index = n % 4;  
+        $(buttons[index]).trigger('click')
+    },1500)
+    //鼠标浮进去暂停
+    $('#window').on('mouseenter',function(){
+        console.log('mouseenter')
+        window.clearInterval(timerId)               //通过清除定时器来取消自动轮播
+    })
+    //鼠标浮出来继续自动轮播
+    $('#window').on('mouseleave',function(){
+        timerId = setInterval(function(){
+            n += 1;
+            let index = n % 4;  
+            $(buttons[index]).trigger('click')    //触发点击事件
+        },1500)
+    })
+}
+
