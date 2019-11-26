69b411e9c47f2d16c69c011e8d4dd6a76894b9e7
holiday <756787204@qq.com>
2019-11-26 22:02:21 +0800

    无缝轮播图优化

--- slide.js
+++ slide.js
@@ -1,26 +1,41 @@
 window.onload = function(){
-    $(`.images>img:nth-child(1)`).addClass('current');
-    $(`.images>img:nth-child(2)`).addClass('enter');
-    $(`.images>img:nth-child(3)`).addClass('enter');
-    $(`.images>img:nth-child(4)`).addClass('enter');
-    let n = 1;
+    let n;
+    initial();
     setInterval(() => {
-        $(`.images>img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
+        makeLeave(getImage(n))
         .one('transitionend',(e) => {
-            $(e.currentTarget).removeClass('leave').addClass('enter')
-        });
-        $(`.images>img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current');
-        n += 1
-    }, 3000);
-    //处理使得n永远是1，2，3，4
+            makeEnter($(e.currentTarget))
+        })
+        makeCurrent(getImage(n+1));
+        n += 1;
+    },1000)
+  
+
+    function getImage(n){
+        return $(`.images > img:nth-child(${x(n)})`)
+    }
     function x(n){
-        if(n>4){         
+        if(n>4){
             n = n%4;
             if(n === 0){
-                n = 4
+                n = 4;
             }
-        }
+        }       // n = 1,2,3,4
         return n
     }
-  
+
+    function initial(){
+        n = 1;
+        $(`.images > img:nth-child(${n})`).addClass('current')
+        .siblings().addClass('enter');
+    }
+    function makeLeave($node){
+        return $node.removeClass('enter current').addClass('leave')
+    }
+    function makeEnter($node){
+        return $node.removeClass('leave current').addClass('enter')
+    }
+    function makeCurrent($node){
+        return $node.removeClass('leave enter').addClass('current')
+    }
 }
